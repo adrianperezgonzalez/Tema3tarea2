d902f5a96d67a9ac736bf7dce017db28d6f7abc3
adrianperezgonzalez <62966837+adrianperezgonzalez@users.noreply.github.com>
2020-05-07 03:16:18 +0200

    Add files via upload

---
+++ entornos_bloque3aev2/Cadena.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entornos_bloque3aev2
+{
+    class Cadena
+    {
+        // Declaracion de variables
+        private string cadenaTexto;
+
+        // Atributos (no se si necesitas esto)
+        public string CadenaTexto { get { return cadenaTexto; } set { cadenaTexto = value; } }
+
+        // Constructor
+        public Cadena(string cadena)
+        {
+            this.cadenaTexto = cadena;
+        }
+
+        /// <summary>
+        /// Devuelve el primer caracter de la cadena de texto
+        /// </summary>
+        /// <returns></returns>
+        public char ExtraerPrimerCaracter()
+        {
+            return this.cadenaTexto[0];
+        }
+
+        /// <summary>
+        /// Devuelve el ultimo caracter de la cadena de texto
+        /// </summary>
+        /// <returns></returns>
+        public char ExtraerUltimoCaracter()
+        {
+            return this.cadenaTexto[cadenaTexto.Length-1];
+        }
+        
+        /// <summary>
+        /// Devuelve el caracter conforme a la posicion pasada por parametro
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public char ExtraerCaracterConcreto(int position)
+        {
+            return this.cadenaTexto[position];
+        }
+
+    }
+}
---
+++ entornos_bloque3aev2/Operacion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entornos_bloque3aev2
+{
+    /// <summary>
+    /// Se encarga de las operaciones (sumas restas y multiplicaciones)
+    /// </summary>
+    class Operacion
+    {
+        // Declaracion de variables
+        private double num1;
+        private double num2;
+        private readonly double CualEsMayor;
+
+        // Atributos (no se si necesitas esto)
+        public double Num1 { get { return num1; } set { num1 = value; } }
+        public double Num2 { get { return num2; } set { num2 = value; } }
+
+        // Constructor
+        public Operacion()
+        {
+            // Inicializamos estableciendo el numero mayor
+            // (no tengo claro si pide un metodo)
+            if (this.num1 >= this.num2) { this.CualEsMayor = num1; }
+            else { this.CualEsMayor = num2; }
+        }
+
+        /// <summary>
+        /// Devuelve la resta el numero menor del mayor y muestra el dia de la semana actual
+        /// </summary>
+        /// <returns></returns>
+        public double Resta()
+        {
+            if (CualEsMayor == this.num1) { return this.num1 -= this.num2; }
+            else { return this.num2 -= this.num1; }
+            // Es necesario llamar al metodo de la clase CosasDeFechas 
+            // para mostrar el dia de la semana actual (Jordi)
+        }
+
+        /// <summary>
+        /// Devuelve la suma de los dos numeros
+        /// </summary>
+        /// <returns></returns>
+        public double Suma()
+        {
+            return this.num1 += this.num2;
+        }
+
+        /// <summary>
+        /// Devuelve la multiplicacion de los dos numeros
+        /// </summary>
+        /// <returns></returns>
+        public double Multiplicacion()
+        {
+            return (this.num1 * this.num2);
+        }
+
+
+    }
+}
